80ec2b7b40c9b7d7560a730a325e3bc8eb0be8e5
Hugo <ledall.hugo.pro@gmail.com>
2019-02-12 11:15:28 +0100

    adding content

---
+++ src/components/Post.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="post col-xs-10 mx-auto mt-3 col-sm-3 col-md-3 align-baseline">
+    <h3 class="title">{{item.title}}</h3>
+    <span>item.timeElapsed</span>
+    <p>item.content</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: ['item']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.post{
+  background-color: #3498db ;
+  height: 30% ;
+}
+.title{
+  background-color: #2980b9 ;
+  overflow: hidden;
+}
+.content {
+  overflow: auto;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav id="nav" class="text-right">
+      <button class="btn" @click="switchView()">Switch to {{view === 0 ? 'list' : 'thumbnails'}}</button>
+    </nav>
+    <div class="container">
+      <HelloWorld :content="content"/>
+    </div>
   </div>
 </template>
 
@@ -13,6 +17,40 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      content: [{ id: 1, abc: 1, content: 1, title: '1235654215634315624210' }, { id: 2, abc: 1, content: 1, title: 2 }, { id: 3, abc: 1, content: 1, title: 2 }],
+      view: 1
+    }
+  },
+  methods: {
+    switchView: function () {
+      this.view = (this.view + 1) % 2
+      fetch('https://www.thermiefrance.com/wp-json/wp/v2/posts')
+        .then((result) => {
+          console.log(result.body.getReader())
+        })
+    }
   }
 }
 </script>
+
+<style lang="scss">
+
+.home {
+  height: 100%;
+  width: 100%;
+}
+.container {
+  height: 100%;
+}
+#nav {
+  padding: 5px ;
+  width:100%;
+  background-color: #2c3e50;
+  button {
+    background-color: #e74c3c;
+  }
+}
+</style>
